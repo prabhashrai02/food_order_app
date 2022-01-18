b4ccc9a1684d1fc3112284807bfffe3e74b8ad39
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-18 23:51:41 +0530

    created basic structure

---
+++ src/Component/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import MainContent from '../MainContent/MainContent';
+
+const HomePage = () => {
+    return(
+        <React.Fragment>  
+            <NavBar />    
+            <MainContent />
+        </React.Fragment>
+    )
+} 
+
+export default HomePage;
---
+++ src/Component/MainContent/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContent = () => {
+    return(
+        <div className='maincontent'>
+
+        </div>
+    )
+} 
+
+export default MainContent;
